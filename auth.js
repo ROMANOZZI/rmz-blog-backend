eec8605d8eca7165cac854bb810f74cd86ddcc6c
ROMANOZZI <mohamedsalah2162000@gmail.com>
2023-09-10 18:57:43 +0300

    keep rolling

--- auth.js
+++ auth.js
@@ -4,8 +4,12 @@ require("dotenv").config();
 const bcrypt = require("bcrypt");
 const database = require("./database");
 const app = express();
+const jwt = require("jsonwebtoken");
 app.use(cors());
 app.use(express.json());
+// registering new users
+/* The code block `app.post("/signup", async (req, res) => { ... })` is defining a route handler for
+the HTTP POST request to "/signup". */
 app.post("/signup", async (req, res) => {
   if (!req.body.email) return res.sendStatus(403);
   const result = await database.checkUserByEmail(req.body.email);
@@ -21,6 +25,33 @@ app.post("/signup", async (req, res) => {
   }
 });
 
+app.post("/login", async (req, res) => {
+  if (!req.body.user) return res.sendStatus(403);
+  const user = req.body.user;
+  if (!user.email || !user.password) return res.sendStatus(500);
+  const data = await database.checkUserByEmail(user.email);
+  if (data[0].length < 1) return res.status(500).send("wrong creditnials");
+  if (await bcrypt.compare(user.password, data[0][0].password)) {
+    const accessToken = jwt.sign(
+      { name: data[0][0].name, email: data[0][0].email },
+      process.env.PRIVATE_KEY,
+      {
+        expiresIn: "10m",
+      }
+    );
+
+    const refreshToken = jwt.sign(
+      { name: data[0][0].name, email: data[0][0].email },
+      process.env.REFRESH_KEY
+    );
+    return res.json({
+      accessToken: accessToken,
+      refreshToken: refreshToken,
+    });
+  }
+
+  return res.status(500).send("something went wrong");
+});
 app.listen(4000, () => {
   console.log("the server is listening on port 4000 sir");
 });
--- database.js
+++ database.js
@@ -7,15 +7,62 @@ const pool = mysql
     database: "blog",
   })
   .promise();
+/**
+ * The function `addUser` is an asynchronous function that inserts a new user into a database table
+ * with the provided name, email, and password.
+ * @param name - The name parameter is the name of the user that you want to add to the database.
+ * @param email - The `email` parameter is a string that represents the email address of the user you
+ * want to add to the database.
+ * @param password - The `password` parameter is the password that the user wants to set for their
+ * account.
+ * @returns The `addUser` function is returning the result of the `pool.query` function, which is a
+ * promise.
+ */
 const addUser = async (name, email, password) => {
   return await pool.query(`
     insert into users (name,email,password) values("${name}","${email}","${password}");
     `);
 };
+/**
+ * The function `checkUserByEmail` queries the database to check if a user with the given email exists.
+ * @param email - The email parameter is a string that represents the email address of a user.
+ * @returns The function `checkUserByEmail` is returning the result of the query executed using the
+ * `pool` object.
+ */
 const checkUserByEmail = async (email) => {
   const res = await pool.query(`select * from users where email= '${email}'`);
 
   return res;
 };
+/**
+ * The addToken function inserts a refresh token and email into a database table called refresh_tokens.
+ * @param Token - The `Token` parameter is a string representing the refresh token that needs to be
+ * added to the database.
+ * @param email - The email parameter is a string that represents the email address of the user for
+ * whom the token is being added.
+ * @returns The `addToken` function is returning the result of the `pool.query` function, which is a
+ * promise.
+ */
+const addToken = async (Token, email) => {
+  return await pool.query(
+    `
+    insert into refresh_tokens (email,token) values ("${email}","${Token}")
+    `
+  );
+};
+/**
+ * The function `checkToken` checks if a given token exists in the `refresh_tokens` table.
+ * @param Token - The `Token` parameter is a string that represents a refresh token. It is used to
+ * query the database and check if the token exists in the `refresh_tokens` table.
+ * @returns The function `checkToken` is returning a boolean value. It will return `true` if there is
+ * at least one row in the `refresh_tokens` table that has a matching token value, and `false`
+ * otherwise.
+ */
+const checkToken = async (Token) => {
+  const res = await pool.query(`
+  select * from refresh_tokens where token=${Token}
+  `);
+  return res[0].length > 0;
+};
 
-module.exports = { checkUserByEmail, addUser };
+module.exports = { checkUserByEmail, addUser, addToken, addToken, checkToken };
